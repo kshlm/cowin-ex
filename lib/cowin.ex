b7441f1360088ab75add3ed51a0a598719bdc070
Kaushal M <kshlmster@gmail.com>
2021-06-13 16:01:04 +0530

    Disable logger for prod

--- lib/cowin.ex
+++ lib/cowin.ex
@@ -8,7 +8,12 @@ defmodule Cowin do
       use Tesla, only: [:get]
       adapter(Tesla.Adapter.Finch, name: Cowin.Finch)
       plug(Tesla.Middleware.BaseUrl, "https://cdn-api.co-vin.in/api")
-      plug(Tesla.Middleware.Headers, [{"user-agent", "cowin-elixir"}, {"Accept-Language", "en_US"}])
+
+      plug(Tesla.Middleware.Headers, [
+        {"user-agent", "cowin-elixir"},
+        {"Accept-Language", "en_US"}
+      ])
+
       plug(Tesla.Middleware.JSON, engine_opts: [keys: :atoms])
       plug(Tesla.Middleware.Logger)
     end
--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Cowin.MixProject do
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      releases: release(),
+      releases: release()
     ]
   end
 
